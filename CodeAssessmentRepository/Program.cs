b3032bddb418afb0d2098c83951b260f46b9be9a
Ari <DonSagiv@gmail.com>
2019-05-05 21:23:33 -0400

    Coderbyte First Factorial Solution

--- CodeAssessmentRepository/Program.cs
+++ CodeAssessmentRepository/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace CodeAssessmentRepository
+namespace Coderbyte.First_Factorial
 {
+    /// <summary>
+    /// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+    /// For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
+    /// For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var integer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FirstFactorial(integer));
+            Console.ReadKey();
+        }
+
+        public static int FirstFactorial(int num)
+        {
+            // 1! = 1
+            int factorialNum = 1;
+
+            // Starts multiplication at 2.
+            // Use dynamic programming to continue multiplication in for loop.
+            for (var i = 2; i <= num; i++)
+                factorialNum = i * factorialNum;
+
+            return factorialNum;
         }
     }
 }
