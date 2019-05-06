08683294760eb1554bd9305427c4d6d1e3e646b5
Ari <DonSagiv@gmail.com>
2019-05-05 21:08:38 -0400

    Add project files.

---
+++ CodeAssessmentRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeAssessmentRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
